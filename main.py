b4e5e55fd04109ec05cc772dc91fd035611dbc6a
Kevin Booth <kbooth@cardinalsolutions.com>
2018-12-20 16:17:02 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,210 @@
+from collections import deque
+
+import cv2
+import sys
+
+import imutils as imutils
+import numpy as np
+
+
+def transform_frame(frame):
+
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    # frame = imutils.resize(frame, width=600)
+    frame = cv2.GaussianBlur(frame, (11, 11), 0)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    greenLower = (29, 86, 6)
+    greenUpper = (64, 255, 255)
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(frame, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    return mask, frame
+
+
+def draw_bounding_box():
+    # Start timer
+    timer = cv2.getTickCount()
+    # Update tracker
+    ok, bbox = tracker.update(frame)
+    # Calculate Frames per second (FPS)
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+
+    # Draw bounding box
+    if ok:
+        # Tracking success
+        p1 = (int(bbox[0]), int(bbox[1]))
+        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+        cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+    else:
+        # Tracking failure
+        cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+    # Display tracker type on frame
+    cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+    # Display FPS on frame
+    cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+    # Display result
+
+
+
+if __name__ == '__main__':
+
+    pts = deque(maxlen=32)
+    counter = 0
+    (dX, dY) = (0, 0)
+    direction = ""
+
+    # Set up tracker.
+    # Instead of MIL, you can also use
+
+    tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[1]
+
+    if tracker_type == 'BOOSTING':
+        tracker = cv2.TrackerBoosting_create()
+    if tracker_type == 'MIL':
+        tracker = cv2.TrackerMIL_create()
+    if tracker_type == 'KCF':
+        tracker = cv2.TrackerKCF_create()
+    if tracker_type == 'TLD':
+        tracker = cv2.TrackerTLD_create()
+    if tracker_type == 'MEDIANFLOW':
+        tracker = cv2.TrackerMedianFlow_create()
+    if tracker_type == 'GOTURN':
+        tracker = cv2.TrackerGOTURN_create()
+
+    if tracker_type == 'MOSSE':
+        tracker = cv2.TrackerMOSSE_create()
+    if tracker_type == "CSRT":
+        tracker = cv2.TrackerCSRT_create()
+
+    # Read video
+    video = cv2.VideoCapture("foos.mp4")
+    length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    print(f'Number of frames {length}')
+
+    start_frame_number = 4300
+    video.set(cv2.CAP_PROP_POS_FRAMES, start_frame_number)
+
+    # Exit if video not opened.
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+
+    mask, frame = transform_frame(frame)
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
+    centerX = bbox[0] + 0.5 * bbox[2];
+    centerY = bbox[1] + 0.5 * bbox[3];
+    center = (centerX, centerY)
+
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
+
+    while True:
+        frame_number = 0
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
+            break
+
+        mask, frame = transform_frame(frame)
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+            # only proceed if the radius meets a minimum size
+            if radius > 10:
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                           (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                pts.appendleft(center)
+
+        # loop over the set of tracked points
+        for i in np.arange(1, len(pts)):
+            # if either of the tracked points are None, ignore
+            # them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+
+            # check to see if enough points have been accumulated in
+            # the buffer
+            if counter >= 10 and i == 1 and pts[-10] is not None:
+                # compute the difference between the x and y
+                # coordinates and re-initialize the direction
+                # text variables
+                dX = pts[-10][0] - pts[i][0]
+                dY = pts[-10][1] - pts[i][1]
+                (dirX, dirY) = ("", "")
+
+                # ensure there is significant movement in the
+                # x-direction
+                if np.abs(dX) > 20:
+                    dirX = "East" if np.sign(dX) == 1 else "West"
+
+                # ensure there is significant movement in the
+                # y-direction
+                if np.abs(dY) > 20:
+                    dirY = "North" if np.sign(dY) == 1 else "South"
+
+                # handle when both directions are non-empty
+                if dirX != "" and dirY != "":
+                    direction = "{}-{}".format(dirY, dirX)
+
+                # otherwise, only one direction is non-empty
+                else:
+                    direction = dirX if dirX != "" else dirY
+
+            thickness = int(np.sqrt(32 / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+            # show the movement deltas and the direction of movement on
+            # the frame
+            cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.65, (0, 0, 255), 3)
+            cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
+                        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.35, (0, 0, 255), 1)
+
+        draw_bounding_box()
+
+        cv2.imshow("Tracking", frame)
+        counter += 1
+
+        # Exit if ESC pressed
+        k = cv2.waitKey(500) & 0xff
+        if k == 27:
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+# cap.open(play.url)
+def track_ball(frame):
+    print(frame)
+
+
+def detect_ball(frame):
+    print('detect')
+
+
